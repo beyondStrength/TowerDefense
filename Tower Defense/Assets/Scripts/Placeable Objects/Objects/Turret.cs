1020dce82ca4ebb353e8d38913313141a5e7075f
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2021-12-03 20:50:10 -0300

    Turret update
    Turret now gets the closest enemy

--- Tower Defense/Assets/Scripts/Placeable Objects/Objects/Turret.cs
+++ Tower Defense/Assets/Scripts/Placeable Objects/Objects/Turret.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class Turret : PlacedObject
@@ -6,16 +7,32 @@ public class Turret : PlacedObject
 
     public int bullets;
 
+    GameObject selectedEnemy;
+
     private void Update()
     {
         Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius);
+        var enemyCollider = enemies.OrderBy(x => (x.transform.position - transform.position).sqrMagnitude).FirstOrDefault();
 
-        // TODO: sort the array to closest enemy
+        selectedEnemy = enemyCollider != null ? enemyCollider.gameObject : null;
+        ShootSelectedEnemy();
+    }
+
+    void ShootSelectedEnemy()
+    {
+        // TODO: check if there is bullets and shoot the enemy
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, radius);
+
+        if (selectedEnemy != null)
+        {
+            // show selected enemy
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, selectedEnemy.transform.position);
+        }
     }
 }
