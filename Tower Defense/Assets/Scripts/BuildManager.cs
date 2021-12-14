bdde1cd92328ab3c3c081d8d62fa47fe177e032d
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2021-12-14 18:14:53 -0300

    Building system update

--- Tower Defense/Assets/Scripts/BuildManager.cs
+++ Tower Defense/Assets/Scripts/BuildManager.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
 
 public class BuildManager : MonoBehaviour
 {
     public static BuildManager i;
 
+    [SerializeField] NavMeshSurface2d navMesh;
+
     Camera mainCamera;
 
-    public PlaceableObject selectedObject;
+    [SerializeField] TMP_Text selectedObjectText;
+
+    [SerializeField] PlaceableObject[] placeableObjects;
+    PlaceableObject selectedObject;
+    int selectedObjectIndex = 0;
 
     private void Awake()
     {
@@ -16,6 +24,9 @@ public class BuildManager : MonoBehaviour
     private void Start()
     {
         mainCamera = Camera.main;
+
+        selectedObject = placeableObjects[selectedObjectIndex];
+        selectedObjectText.text = selectedObject.name;
     }
 
     private void Update()
@@ -25,6 +36,28 @@ public class BuildManager : MonoBehaviour
             Vector2 point = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
             Instantiate(selectedObject.prefab, point, Quaternion.identity);
+            navMesh.BuildNavMesh();
+        }
+
+        if(Input.mouseScrollDelta == Vector2.up)
+        {
+            selectedObjectIndex--;
+
+            if (selectedObjectIndex <= 0)
+                selectedObjectIndex = 0;
+
+            selectedObject = placeableObjects[selectedObjectIndex];
+            selectedObjectText.text = selectedObject.name;
+        }
+        else if(Input.mouseScrollDelta == Vector2.down)
+        {
+            selectedObjectIndex++;
+
+            if (selectedObjectIndex >= placeableObjects.Length - 1)
+                selectedObjectIndex = placeableObjects.Length - 1;
+
+            selectedObject = placeableObjects[selectedObjectIndex];
+            selectedObjectText.text = selectedObject.name;
         }
     }
 }
