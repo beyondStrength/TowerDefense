70182eb4bbcac1317ed2f38fd86f6d098e63cc79
beyondStrength <61022493+beyondStrength@users.noreply.github.com>
2021-12-14 12:33:02 -0300

    Setting up navmesh to ground and navmesh agent to enemy

--- Tower Defense/Assets/Scripts/Enemy.cs
+++ Tower Defense/Assets/Scripts/Enemy.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [Range(0, 100)] public int health = 100;
+    NavMeshAgent nav = null!;
+    
 
-    void Start()
+    void Awake()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
+    }
+    private void Start() {
+        nav.updateRotation = false;
+        nav.updateUpAxis = false;
+        nav.SetDestination(new Vector3(7,4,0));
     }
 
     void Update()
     {
-        
+        //TODO: destroy on life < 0
     }
+
+    //TODO: show GUI life
+    
 }
